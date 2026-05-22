4f77c84916693afc5c25373fee463ded0b43695f
maneboy <okonudumanasseh@gmail.com>
2026-05-22 00:39:16 +0000

    initial commit

---
+++ src/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the home page");
+});
+
+app.listen(PORT, () => {
+  console.log(`server running on ${PORT}`);
+});
